c3441d65fea0151fb48d881b764c2d15dbee8624
asterisc0 <149898621+asterisc0@users.noreply.github.com>
2023-11-21 20:02:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrape() {
+    // Aquí agregarás tu código JavaScript para realizar el web scraping y mostrar la información.
+    // Puedes usar fetch para obtener el contenido de la URL y luego parsearlo para extraer la información.
+    // Recuerda manejar las acciones aleatorias y actualizar el contenido cuando se reinicie la página.
+    const url = document.getElementById('urlInput').value;
+
+    // Realizar el web scraping y mostrar la información en #infoDisplay
+    // ...
+
+    // Ejemplo de actualización del contenido
+    document.getElementById('infoDisplay').innerHTML = '<p>Información obtenida:</p><p>...</p>';
+}
